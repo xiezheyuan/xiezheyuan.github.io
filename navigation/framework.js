2d742b547d8b943c0d52dae88f7906db4ea6dd3f
xiezheyuan <102935106+xiezheyuan@users.noreply.github.com>
2025-02-16 09:47:37 +0000

    initial navigation index.html

---
+++ navigation/framework.js
@@ -0,0 +1,37 @@
+const generateHTML = (data) => {
+    const { title, summary, links } = data;
+    let html = `<h2 class="ui header">${title}</h2>`;
+    html += `<div class="ui divider"></div>`;
+    html += `<div class="ui relaxed divided list">`;
+    links.forEach(link => {
+        html += `
+            <div class="item">
+                <i class="large ${link.icon} middle aligned icon"></i>
+                <div class="content">
+                    <a class="header" href="${link.url}" target="_blank">${link.title}</a>
+                    <div class="description">${summary}</div>
+                </div>
+            </div>`;
+    });
+    html += `</div>`;
+    html += `<div class="ui divider"></div>`;
+    return html;
+}
+
+
+const build = (settings) => {
+    // header
+    const header = settings.header;
+    document.getElementById('header').innerHTML = header;
+    // sections
+    const sectionsContainer = document.getElementById('sections');
+    sectionsContainer.innerHTML = '';
+    settings.sections.forEach((section) => {
+        const html = generateHTML(section);
+        sectionsContainer.innerHTML += html + '\n';
+    });
+    // footer
+    const footer = settings.footer;
+    document.getElementById('footer').innerHTML = footer;
+    document.title = settings.title;
+};
---
+++ navigation/settings.js
@@ -0,0 +1,18 @@
+build({
+    header: 'Welcome to my website!',
+    sections: [
+        {
+            title: 'Sites',
+            summary: 'There are sites about me.',
+            links: [
+                {
+                    title: 'My Site 1',
+                    url: 'https://mysite1.com',
+                    icon: 'github'
+                }
+            ]
+        }
+    ],
+    footer: 'Powered by xzy.',
+    title: 'My Website'
+});
