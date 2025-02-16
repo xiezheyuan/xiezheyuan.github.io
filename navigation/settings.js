6bb9dbfd998f78d922639dd87a361a629d0a1649
xiezheyuan <102935106+xiezheyuan@users.noreply.github.com>
2025-02-16 10:30:52 +0000

    update

--- navigation/settings.js
+++ navigation/settings.js
@@ -1,18 +1,29 @@
 build({
-    header: 'Welcome to my website!',
+    header: '欢迎！这里是 xzy 的主页',
     sections: [
         {
-            title: 'Sites',
-            summary: 'There are sites about me.',
+            title: '寻找 xzy',
+            summary: 'xzy 可能活跃的平台',
             links: [
                 {
-                    title: 'My Site 1',
-                    url: 'https://mysite1.com',
-                    icon: 'github'
+                    title: 'Luogu',
+                    url: 'https://www.luogu.com.cn/user/413065',
+                    icon: 'book'
+                }
+            ]
+        },
+        {
+            title: '工具',
+            summary: '列出一些 xzy 制作的实用工具',
+            links: [
+                {
+                    title: '括号序列-树 研究工具',
+                    url: './tools/bracket-tree.html',
+                    icon: 'terminal'
                 }
             ]
         }
     ],
     footer: 'Powered by xzy.',
-    title: 'My Website'
+    title: 'xzy 的主页'
 });
